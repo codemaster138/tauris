b0d491ee5693aac953177b97d3466af18d9a33b4
Jake <pygamer138@gmail.com>
2022-03-31 18:39:53 +0200

    chore: compile & publish

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -308,8 +319,11 @@ var Command = /** @class */ (function () {
      * @param cmd Subcommand to attach
      */
     Command.prototype.command = function (cmd) {
+        var _a, _b;
         cmd.parent = this;
         cmd.options = cmd.options.concat(this.options.filter(function (x) { return x.isRoot; }));
+        if (!((_a = cmd.opts) === null || _a === void 0 ? void 0 : _a.language))
+            cmd.opts = __assign(__assign({}, (cmd.opts || {})), { language: (_b = this.opts) === null || _b === void 0 ? void 0 : _b.language });
         this.subcommands.push(cmd);
         return this;
     };

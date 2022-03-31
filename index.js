bad11167a766dca869ec494a8d3a8640bc5ee982
Jake <pygamer138@gmail.com>
2022-03-31 17:42:06 +0200

    chore: compile & publish

--- index.js
+++ index.js
@@ -12,6 +12,42 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Command = exports.UsageError = void 0;
 var chalk_1 = require("chalk");
@@ -86,7 +122,7 @@ var Command = /** @class */ (function () {
         return this;
     };
     /**
-     * Parse arguments into an object
+     * Parse arguments into an object. If a command is called, returns a promise so you can await the command.
      * @param argv Raw process arguments, without binary and file location (e.g. `process.argv.slice(2)`)
      */
     Command.prototype.parse = function (argv, options) {
@@ -132,16 +168,16 @@ var Command = /** @class */ (function () {
                 }
             }
             else {
-                var found_1 = false;
-                this_1.subcommands.forEach(function (cmd) {
+                var promise = this_1.subcommands
+                    .map(function (cmd) {
                     if (cmd.name === stripped) {
-                        found_1 = true;
-                        cmd.callHandler(cmd.parse(argv.slice(index + 1)));
+                        return cmd.callHandler(cmd.parse(argv.slice(index + 1), { noPromise: options === null || options === void 0 ? void 0 : options.noPromise, noExit: options === null || options === void 0 ? void 0 : options.noExit }));
                     }
-                });
-                if (found_1) {
-                    return { value: false };
-                }
+                    return null;
+                })
+                    .find(function (x) { return !!x; });
+                if (promise)
+                    return { value: (options === null || options === void 0 ? void 0 : options.noPromise) ? false : promise };
                 else
                     Array.isArray(res.parameters)
                         ? res.parameters.push(stripped)
@@ -239,8 +275,22 @@ var Command = /** @class */ (function () {
         return this.parent ? this.parent.root() : this;
     };
     Command.prototype.callHandler = function (argv) {
-        if (argv)
-            this._handler(argv);
+        return __awaiter(this, void 0, void 0, function () {
+            var args;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, argv];
+                    case 1:
+                        args = _a.sent();
+                        if (!args) return [3 /*break*/, 3];
+                        return [4 /*yield*/, this._handler(args)];
+                    case 2:
+                        _a.sent();
+                        _a.label = 3;
+                    case 3: return [2 /*return*/];
+                }
+            });
+        });
     };
     return Command;
 }());
